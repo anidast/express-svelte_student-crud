dded182045f7be487914d465bf4d623f65d8e565
anidast <anidwi.ad1@gmail.com>
2020-11-19 18:59:02 +0700

    modal success & failed

--- frontend/src/containers/form-student/index.svelte
+++ frontend/src/containers/form-student/index.svelte
@@ -2,11 +2,24 @@
   import "./index.scss";
   import type { Student } from "../../types/student.type";
   import type { Major } from "../../types/major.type";
-  import { majorsList, studentEdit } from "../../stores";
+  import { majorsList } from "../../stores";
   import axios from "axios";
 
   export let student: Student;
   export let updateStatus: boolean;
+
+  $: modalSuccessStatus = false;
+  $: modalFailedStatus = false;
+  let modalText: string = "";
+
+  function closeSuccessModal() {
+    modalSuccessStatus = false;
+    window.location.href = "/";
+  }
+
+  function closeFailedModal() {
+    modalFailedStatus = false;
+  }
 
   function submitStudent() {
     console.log(student);
@@ -15,10 +28,14 @@
         .post("http://localhost:4000/students/add", student)
         .then((response) => {
           console.log(response.data);
-          window.location.href = "/";
+          modalText = response.data;
+          modalSuccessStatus = true;
+          // window.location.href = "/";
         })
         .catch((error) => {
           console.log(error);
+          modalText = error;
+          modalFailedStatus = true;
         });
     }
     else{
@@ -87,6 +104,28 @@
       <button class="button is-link my-4" on:click={submitStudent}>
         Submit Student
       </button>
+    </div>
+  </div>
+  <div class="modal" class:is-active={modalSuccessStatus}>
+    <div class="modal-background" on:click={closeSuccessModal}></div>
+    <div class="modal-content">
+      <div class="notification is-info">
+        <button class="delete" on:click={closeSuccessModal}></button>
+        <p class="is-size-3 has-text-weight-bold has-text-centered">
+          {modalText}
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="modal" class:is-active={modalFailedStatus}>
+    <div class="modal-background" on:click={closeFailedModal}></div>
+    <div class="modal-content">
+      <div class="notification is-danger">
+        <button class="delete" on:click={closeFailedModal}></button>
+        <p class="is-size-3 has-text-weight-bold has-text-centered">
+          {modalText}
+        </p>
+      </div>
     </div>
   </div>
 </section>
